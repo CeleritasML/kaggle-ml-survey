ac485f5b75b8a3857cf5441a210f5df19920bf77
gaojingsong <jg2109@georgetown.edu>
2021-11-29 18:17:03 -0500

    feat: Q5-8 vis part

--- scripts/Part2-1.R
+++ scripts/Part2-1.R
@@ -8,32 +8,89 @@ dat <- read_csv('data/kaggle_survey_2021_responses.csv', col_names = col_names, 
 
 # data analyst, data engineer, data scientist, machine learning engineer, software engineer, statistician
 
-us.dat <- dat %>% filter(Q3 == "United States of America")
-job.dat <- us.dat %>% filter(Q5 %in% c("Data Analyst", 
-                                       "Data Engineer",
-                                       "Data Scientist",
-                                       "Machine Learning Engineer",
-                                       "Software Engineer",
-                                       "Statistician"))
+dat <- dat %>% filter(Q3 == "United States of America")
+job.dat <- dat %>% 
+    filter(Q5 %in% c("Data Analyst",
+                     "Data Engineer",
+                     "Data Scientist",
+                     "Machine Learning Engineer",
+                     "Software Engineer",
+                     "Statistician",
+                     "Student"))
 
 programming <- job.dat %>% 
     select(c(Q5, starts_with("Q7_"))) %>%
     gather("fake_key", "language", -Q5, na.rm = T) %>%
     rename(title = Q5) %>%
-    select(-fake_key)
+    select(-fake_key) %>%
+    filter(!language %in% c("None", "Other")) %>%
+    count(title, language, .drop = FALSE) %>% 
+    complete(title, language) %>%
+    replace_na(list(n = 0)) %>%
+    group_by(title) %>%
+    mutate(prop = prop.table(n))
+
+p <- programming %>% 
+    mutate(text = paste0("Language: ", language, "\n", 
+                         "Job title: ", title, "\n", 
+                         "Count: ", n, "\n",
+                         "Proportion: ", round(prop, 3))) %>%
+    ggplot(aes(language, title, fill=prop, text=text)) +
+    geom_tile() +
+    scale_fill_gradient(low="white", high="blue") +
+    labs(
+        title = "Users' favorite programming language",
+        caption = glue("Author: celeritasML
+                   Source: Kaggle")) +
+    theme(axis.ticks.x = element_blank(),
+          axis.text.x = element_text(angle=90, hjust=1),
+          axis.title = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())
+ggplotly(p, tooltip="text")
 
 recommend <- job.dat %>% 
     select(c(Q5, Q8)) %>%
     gather("fake_key", "recommend.language", -Q5, na.rm = T) %>%
     rename(title = Q5) %>%
-    select(-fake_key)
+    select(-fake_key) %>%
+    filter(!recommend.language %in% c("None", "Other"))
+table(recommend)
 
 ide <- job.dat %>% 
     select(c(Q5, starts_with("Q9_"))) %>%
     gather("fake_key", "IDE", -Q5, na.rm = T) %>%
     rename(title = Q5) %>%
-    select(-fake_key)
+    select(-fake_key) %>%
+    mutate(IDE = case_when(
+        IDE == "Visual Studio Code (VSCode)" ~ "VSCode",
+        IDE == "Jupyter (JupyterLab, Jupyter Notebooks, etc)" ~ "Jupyter Notebook",
+        TRUE ~ IDE
+    )) %>%
+    filter(!IDE %in% c("None", "Other")) %>%
+    count(title, IDE, .drop = FALSE) %>% 
+    complete(title, IDE) %>%
+    replace_na(list(n = 0)) %>%
+    group_by(title) %>%
+    mutate(prop = prop.table(n))
 
-programming %>% table()
-recommend %>% table()
-ide %>% table()
+p <- ide %>% 
+    mutate(text = paste0("IDE: ", IDE, "\n", 
+                         "Job title: ", title, "\n", 
+                         "Count: ", n, "\n",
+                         "Proportion: ", round(prop, 3))) %>%
+    ggplot(aes(IDE, title, fill=prop, text=text)) +
+    geom_tile() +
+    scale_fill_gradient(low="white", high="blue") +
+    labs(
+        title = "Users' favorite IDE",
+        caption = glue("Author: celeritasML
+                   Source: Kaggle")) +
+    theme(axis.ticks.x = element_blank(),
+          axis.text.x = element_text(angle=90, hjust=1),
+          axis.title = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())
+ggplotly(p, tooltip="text")
+table(ide)
+
